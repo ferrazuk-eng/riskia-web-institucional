67038d1238d2a631418d7a7d4b1e7d1214aae509
Rick <rickbot@server.local>
2026-07-27 08:01:02 -0300

    Radar Update (Auto A5) - 27/07/2026 08:01

--- live_data.js
+++ live_data.js
@@ -1,11 +1,11 @@
 /** 
  * RISKIA AUTO-GENERATED DATA 
- * Session: 24/07/2026 17:32
+ * Session: 27/07/2026 08:01
  * Motor: web_sync_v5.0
  */
 
 const RISKIA_LIVE_DATA = {
-    "update_time": "24/07/2026 17:32",
+    "update_time": "27/07/2026 08:01",
     "macro": [
         {
             "label": "SPY",
