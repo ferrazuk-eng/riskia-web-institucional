4830e3059b44e82ad0fe7724dd88c7326ee1917e
Rick <rickbot@server.local>
2026-07-30 09:57:37 -0300

    Radar Update (Auto A5) - 30/07/2026 09:57

--- live_data.js
+++ live_data.js
@@ -1,26 +1,26 @@
 /** 
  * RISKIA AUTO-GENERATED DATA 
- * Session: 30/07/2026 08:50
+ * Session: 30/07/2026 09:57
  * Motor: web_sync_v5.0
  */
 
 const RISKIA_LIVE_DATA = {
-    "update_time": "30/07/2026 08:50",
+    "update_time": "30/07/2026 09:57",
     "macro": [
         {
             "label": "SPY",
-            "value": "-0.12%",
+            "value": "-0.90%",
             "color": "red"
         },
         {
             "label": "QQQ",
-            "value": "-0.00%",
-            "color": "emerald"
+            "value": "-0.41%",
+            "color": "red"
         },
         {
             "label": "VIX",
-            "value": "20.66",
-            "color": "amber"
+            "value": "19.14",
+            "color": "emerald"
         },
         {
             "label": "Dolar CCL",
@@ -29,7 +29,7 @@ const RISKIA_LIVE_DATA = {
         },
         {
             "label": "Riesgo Pais",
-            "value": "442",
+            "value": "441",
             "color": "riskiaGray"
         }
     ],
