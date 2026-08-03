3499a10c0a5cb8f9bd9ab40eda7018f2cec98026
ferrazuk-eng <fer.razuk@gmail.com>
2026-08-03 14:28:35 -0300

    Radar Update

--- live_data.js
+++ live_data.js
@@ -1,30 +1,30 @@
 /** 
  * RISKIA AUTO-GENERATED DATA 
- * Session: 03/08/2026 09:31
+ * Session: 03/08/2026 14:10
  * Motor: web_sync_v4.6
  */
 
 const RISKIA_LIVE_DATA = {
-    "update_time": "03/08/2026 09:31",
+    "update_time": "03/08/2026 14:10",
     "macro": [
         {
             "label": "SPY",
-            "value": "+1.17%",
+            "value": "+1.25%",
             "color": "emerald"
         },
         {
             "label": "QQQ",
-            "value": "+0.68%",
+            "value": "+1.52%",
             "color": "emerald"
         },
         {
             "label": "VIX",
-            "value": "16.05",
+            "value": "15.56",
             "color": "emerald"
         },
         {
             "label": "Dolar CCL",
-            "value": "$1575.7",
+            "value": "$1578.2",
             "color": "blue"
         },
         {
